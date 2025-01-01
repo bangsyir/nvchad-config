f5f1d17d696606de4113761633e58e54a2ac2a70
bangsyir <bangsyir@gmail.com>
2025-01-01 22:31:54 +0800

    add normal mode move line up and down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,10 @@ map(
   "<cmd>lua require('kulala').run()<cr>",
   { noremap = true, silent = true, desc = "Execute the request" }
 )
-map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
-map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+-- normal mode
+map("n", "<A-j>", "mz:m .+1<CR>==`z", { desc = "Move line down" })
+map("n", "<A-k>", "mz:m .-2<CR>==`z", { desc = "Move line up" })
+-- visual mode
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" }) -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" }) -- move line down(v)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
