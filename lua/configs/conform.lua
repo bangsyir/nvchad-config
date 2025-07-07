5f04dc02b5caf35fd7e41f887048e6705fee1e8d
bangsyir <bangsyir@gmail.com>
2025-07-07 21:34:56 +0800

    add prettier for typescript

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    typescript = {"prettier"},
-    json = {"prettier"}
+    css = { "prettierd" },
+    html = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    json = { "prettierd" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 1000,
+    timeout_ms = 2000,
+    -- async = true,
     lsp_fallback = true,
   },
 }
