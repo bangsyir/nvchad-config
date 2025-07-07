469da318f957014a31b7b6f58b63863edcd77f0e
bangsyir <bangsyir@gmail.com>
2025-07-07 21:36:02 +0800

    add lsp : prismals, unocss, gopls and pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua", "prismals", "ts_ls", "gopls" }
+local servers = { "html", "cssls", "lua", "prismals", "ts_ls", "unocss", "gopls", "pyright" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
